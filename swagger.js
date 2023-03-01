b34b492cbd930ac634cf9432c16079c0f0625aad
Helaman Rodrigues <u711602997@byui.edu>
2023-02-28 23:58:52 -0300

    update assignment 04

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Temple API',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
+  host: 'cse341-assignment04.onrender.com/contacts',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
