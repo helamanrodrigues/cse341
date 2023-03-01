e03410aa1785f396ecc3effc0f22786941654f0a
Helaman Rodrigues <u711602997@byui.edu>
2023-03-01 20:15:46 -0300

    change the title of assignment 04

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API',
+    description: 'Contacts API',
   },
   host: 'cse341-assignment04.onrender.com',
   schemes: ['https'],
