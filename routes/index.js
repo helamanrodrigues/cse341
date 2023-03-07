656198c2e5f5c5fd3482e198dc2aa63ce9bf045f
Helaman Rodrigues <u711602997@byui.edu>
2023-03-07 20:21:38 -0300

    fixing errors

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,14 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/heroes', require('./heroes'));
+router.use(
+    '/',
+    (docData = (req, res) => {
+        let docData = {
+            documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+        };
+        res.send(docData);
+    })
+);
 
 module.exports = router;
